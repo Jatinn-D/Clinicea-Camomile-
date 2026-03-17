dbcf2b41b09e8fbd54c4080f5ff221b6f0c86d1c
Jatinn-D <jd0287@srmist.edu.in>
2026-03-17 23:17:42 +0530

    updated config file

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,6 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
+// In a file like client/src/api/config.js
+const API_BASE_URL = window.location.hostname === 'localhost' 
+  ? 'http://localhost:5000' 
+  : 'https://clinicea-camomile-production.up.railway.app';
+
+export default API_BASE_URL;
